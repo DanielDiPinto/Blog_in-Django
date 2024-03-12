d90631ac603896d463bdac57ab06e2e9fa2b3449
Danieldi25 <ddp251019@gmail.com>
2024-03-12 11:56:02 +0100

    sezione post finita

--- posts/urls.py
+++ posts/urls.py
@@ -3,15 +3,16 @@ from . import views as post_view
 from django.views.generic import ListView, DeleteView
 from .models import Post
 
+
 urlpatterns = [
     path('', ListView.as_view(
         queryset = Post.objects.all().order_by("-date"),
         template_name = 'lista_post.html',
-        paginate_by = 5), name='lista'),
+        paginate_by = 6), name='lista'),
         
     
     path('<int:id>/<slug:slug>/', DeleteView.as_view(
         model = Post,
         template_name = 'post_singolo.html',), name='singolo'),
-    path('', post_view.contatti, name='contatti'),
+    path('contatti/', post_view.contatti, name='contatti'),
 ]
